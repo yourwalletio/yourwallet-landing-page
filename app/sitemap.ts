dd78379f97ebb059132eb9a95b93b9f97d38e835
SerkanYalçınkaya <syalcinkaya895@gmail.com>
2025-01-28 00:57:10 +0300

    feat: changed sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,50 +1,48 @@
-import { MetadataRoute } from 'next'
+import type { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://yourwallet.tr'
-  
   return [
     {
-      url: baseUrl,
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
+      url: 'https://yourwallet.tr',
+      lastModified: new Date().toISOString(),
+      changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: `${baseUrl}/about-us`,
-      lastModified: new Date(),
+      url: 'https://yourwallet.tr/about-us',
+      lastModified: new Date().toISOString(),
       changeFrequency: 'monthly',
-      priority: 0.8,
+      priority: 0.9,
     },
     {
-      url: `${baseUrl}/download`,
-      lastModified: new Date(),
+      url: 'https://yourwallet.tr/download',
+      lastModified: new Date().toISOString(),
       changeFrequency: 'weekly',
       priority: 0.9,
     },
     {
-      url: `${baseUrl}/privacy-policy`,
-      lastModified: new Date(),
+      url: 'https://yourwallet.tr/privacy-policy',
+      lastModified: new Date().toISOString(),
       changeFrequency: 'monthly',
       priority: 0.5,
     },
     {
-      url: `${baseUrl}/terms-of-service`,
-      lastModified: new Date(),
+      url: 'https://yourwallet.tr/terms-of-service',
+      lastModified: new Date().toISOString(),
       changeFrequency: 'monthly',
       priority: 0.5,
     },
     {
-      url: `${baseUrl}/support`,
-      lastModified: new Date(),
+      url: 'https://yourwallet.tr/support',
+      lastModified: new Date().toISOString(),
       changeFrequency: 'weekly',
-      priority: 0.8,
+      priority: 0.9,
     },
     {
-      url: `${baseUrl}/swap`,
-      lastModified: new Date(),
+      url: 'https://yourwallet.tr/swap',
+      lastModified: new Date().toISOString(),
       changeFrequency: 'weekly',
       priority: 0.9,
     },
   ]
-} 
+}
